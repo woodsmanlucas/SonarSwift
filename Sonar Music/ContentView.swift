07e99e725a209d18414dd7102fb605b6045d2378
Lucas Johnson <woodsman.lucas@gmail.com>
2020-08-15 07:27:00 -0700

    Initial Commit

---
+++ Sonar Music/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sonar Music
+//
+//  Created by Lucas Johnson on 2020-08-15.
+//  Copyright © 2020 Sonar Music. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
